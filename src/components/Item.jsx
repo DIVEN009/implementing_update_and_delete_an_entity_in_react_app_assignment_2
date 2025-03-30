7e785b93c9036ffdb0b41ef9e192b023cccda1f7
DIVEN009 <sainidiven@gmail.com>
2025-03-30 12:25:51 +0530

    Done

--- src/components/Item.jsx
+++ src/components/Item.jsx
@@ -1,8 +1,23 @@
-const Item = ({ item }) => {
+import PropTypes from "prop-types";
 
-    // Render a single item
-    // Add a Delete and Edit button
-    return null;
+const Item = ({ item, onDelete }) => {
+  console.log("Item received:", item); // Debugging log
+
+  return (
+    <div className="item-card">
+      <h3>{item.name}</h3>
+      <button onClick={() => onDelete(item.id)}>Delete</button>
+      <button>Edit</button>
+    </div>
+  );
+};
+
+Item.propTypes = {
+  item: PropTypes.shape({
+    id: PropTypes.oneOfType([PropTypes.string, PropTypes.number]).isRequired,
+    name: PropTypes.string.isRequired,
+  }).isRequired,
+  onDelete: PropTypes.func.isRequired,
 };
 
 export default Item;
--- src/components/ItemList.jsx
+++ src/components/ItemList.jsx
@@ -1,14 +1,26 @@
 import Item from "./Item";
+import PropTypes from "prop-types";
 
-const ItemList = ({ items }) => {
-    // your code here
-    return (
-        <>
-            {items.map((item) => (
-                <Item key={item.id} item={item} />
-            ))}
-        </>
-    );
+const ItemList = ({ items, onDelete }) => {
+  return (
+    <>
+      {items.length > 0 ? (
+        items.map((item) => <Item key={item.id} item={item} onDelete={onDelete} />)
+      ) : (
+        <p>No items found.</p>
+      )}
+    </>
+  );
+};
+
+ItemList.propTypes = {
+  items: PropTypes.arrayOf(
+    PropTypes.shape({
+      id: PropTypes.oneOfType([PropTypes.string, PropTypes.number]).isRequired,
+      name: PropTypes.string.isRequired,
+    })
+  ).isRequired,
+  onDelete: PropTypes.func.isRequired,
 };
 
 export default ItemList;
